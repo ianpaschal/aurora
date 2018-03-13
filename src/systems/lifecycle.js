89b46f1a39c0fe47f1967460cd0b350984f65f7e
Ian Paschal <i.paschal@ultimaker.com>
2018-03-13 23:55:59 +0100

    Clean-up
    - Removed depreciated files
    - Updated documentation

--- src/systems/lifecycle.js
+++
@@ -1,4 +0,0 @@
-/** This system is responsible for handling the spawning and despawning of entities when they are born or die.
-
-	this should check each entity in the registry if there is also a mesh with that entity's uuid set. if not, generate one.
-*/
