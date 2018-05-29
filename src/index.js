a38436d7b02e505dcd009ddb68eb872795f8c661
Ian Paschal <ian.paschal@gmail.com>
2018-05-29 09:30:15 +0200

    Added JSONLoader

--- src/index.js
+++ src/index.js
@@ -7,15 +7,29 @@ console.warn(
 	"See more tips at https://ianpaschal.github.io/aurora/guide/deployment"
 );
 
+// Core
 export { default as Component } from "./core/Component";
 export { default as Engine } from "./core/Engine";
 export { default as Entity } from "./core/Entity";
 export { default as Player } from "./core/Player";
+export { default as State } from "./core/State";
 export { default as System } from "./core/System";
 
+// Loaders
+export { default as EntityLoader } from "./loaders/EntityLoader";
+export { default as JSONLoader } from "./loaders/JSONLoader";
+export { default as MaterialLoader } from "./loaders/MaterialLoader";
+
+// Managers
+export { default as PluginManager } from "./managers/PluginManager";
+export { default as StateManager } from "./managers/StateManager";
+
+// Math
 export { default as isBetween } from "./math/isBetween";
 export { default as Math2D } from "./math/Math2D";
 export { default as Math3D } from "./math/Math3D";
 export { default as Percentage } from "./math/Percentage";
+
+// Utils
 import * as utils from "./utils";
 export { utils };
---
+++ src/loaders/JSONLoader.js
@@ -0,0 +1,55 @@
+// Aurora is distributed under the MIT license.
+
+import FS from "fs";
+import Entity from "../core/Entity";
+import Percentage from "../math/Percentage";
+
+/** @classdesc Core singleton representing an instance of the Aurora Engine. The
+	* engine is responsible for the creation (and registration) of entities, as
+	* well as initialization and running of systems containing game logic.
+	* @returns - The newly created Engine.
+	*/
+class JSONLoader {
+
+	/** @description Load an Entity from a JSON file.
+		* @param {String} src - Filepath to load.
+		* @param {Function} onLoad - Callback to execute on successful load.
+		* @param {Function} onProgress - Callback to execute on progress.
+		* @param {Function} onError - Callback to execute on error.
+		* @returns {Entity} - A new Enity instance loaded from JSON.
+		*/
+	load( src, onLoad, onProgress, onError ) {
+		if ( typeof onLoad !== "function" ) {
+			return;
+		}
+		if ( typeof onError !== "function" ) {
+			return;
+		}
+
+		const stats = FS.statSync( src );
+		const stream = FS.createReadStream( src, { highWaterMark: 16 });
+		const chunks = [];
+
+		// Handle any errors while reading
+		stream.on( "error", ( err ) => {
+			return onError( err );
+		});
+
+		// Listen for data
+		stream.on( "data", ( chunk ) => {
+			chunks.push( chunk );
+			if ( typeof onProgress === "function" ) {
+				onProgress( new Percentage( stream.bytesRead / stats.size ) );
+			}
+		});
+
+		// File is done being read
+		stream.on( "close", () => {
+			// Create a buffer of the chunks from the stream
+			const json = JSON.parse( Buffer.concat( chunks ) );
+			return onLoad( json );
+		});
+	}
+}
+
+export default JSONLoader;
