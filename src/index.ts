726a4c8776d2ac2d1d9502a718ba1973b83159e5
Ian Paschal <ian.paschal@gmail.com>
2018-09-05 23:15:01 +0200

    Reduce testing scope (temporarily)

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ export { default as State } from "./core/State";
 export { default as System } from "./core/System";
 
 // Math
+export { default as interpolate } from "./math/interpolate";
 export { default as isBetween } from "./math/isBetween";
 export { default as math2D } from "./math/math2D";
 export { default as math3D } from "./math/math3D";
