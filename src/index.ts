d1d9803106d756bb76fae667b387437eae3d0274
Ian Paschal <projects@ianpaschal.com>
2018-09-30 23:09:26 +0200

    Remove false message

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 // Aurora is distributed under the MIT license.
 
-// This is removed via Parcel when creating a production bundle
-console.warn(
-	"If you are seeing this message, you are running Aurora in development mode.",
-	"Make sure to turn on production mode when deploying for production.",
-	"See more tips at https://ianpaschal.github.io/aurora/guide/deployment"
-);
-
 // Core
 export { default as Component } from "./core/Component";
 export { default as Engine } from "./core/Engine";
